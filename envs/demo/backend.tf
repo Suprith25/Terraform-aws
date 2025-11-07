c3ba4121485058d4c2b3e25bc218a8a884a88151
Suprith Gowda <gowdajsuprith44@gmail.com>
2025-11-07 12:20:53 +0530

    s3 bucket creation

--- envs/demo/backend.tf
+++ envs/demo/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket               = "your-unique-tfstate-bucket"   # <-- REPLACE
-    key                  = "envs/demo/terraform.tfstate"
-    region               = "ap-south-1"
-    encrypt              = true
-    workspace_key_prefix = "env:"
-    use_lockfile         = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket               = "your-unique-tfstate-bucket" # <-- REPLACE
+#    key                  = "envs/demo/terraform.tfstate"
+#    region               = "ap-south-1"
+#    encrypt              = true
+#    workspace_key_prefix = "env:"
+#    use_lockfile         = true
+#  }
+#}
--- envs/demo/main.tf
+++ envs/demo/main.tf
@@ -3,16 +3,29 @@ variable "environment" {
   default = "demo"
 }
 
-module "vpc" {
-  source      = "../../modules/vpc"
-  cidr_block  = "10.0.0.0/16"
-  environment = var.environment
+#/*
+#module "vpc" {
+#  source      = "../../modules/vpc"
+#  cidr_block  = "10.0.0.0/16"
+#  environment = var.environment
+#}
+#
+#module "compute" {
+#  source         = "../../modules/s3"
+#  subnet_ids     = module.vpc.public_subnet_ids
+#  environment    = var.environment
+#  instance_type  = "t2.micro"
+#  instance_count = 1
+#}
+#*/
+
+variable "bucket_name" {
+  type        = string
+  description = "Globally unique S3 bucket name"
 }
 
-module "compute" {
-  source         = "../../modules/compute"
-  subnet_ids     = module.vpc.public_subnet_ids
-  environment    = var.environment
-  instance_type  = "t2.micro"
-  instance_count = 1
+module "s3_bucket" {
+  source      = "../../modules/s3"
+  bucket_name = var.bucket_name
+  environment = var.environment
 }
